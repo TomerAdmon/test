cf706c69ea4ab2326b010c0f2848af06d8532bff
Tomer Admon <tomer.a@qualisystems.com>
2017-06-08 14:20:50 +0300

    Create cloudshell_api_example.py

--- cloudshell_api_example.py
+++ cloudshell_api_example.py
@@ -11,11 +11,4 @@ session = CloudShellAPISession(server_ip,
                                sys.argv[5])  ##make sure 
 										  ##to pass these credentials from jenkins and don't store them in GitHub!!
 
-resources = session.GetReservationDetails(reservation_id).ReservationDescription.Resources
-my_resource = [resource for resource in resources
-               if resource.ResourceModelName == DEPLOYED_APP_MODEL]
-
-if len(my_resource) > 1:
-    raise Exception('There are more then one app in the sandbox')
-
-print my_resource[0].FullAddress
+print session.GetUserDetails('admin').Name
