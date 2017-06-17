56e3914868c86edcd7242f2b93446c4178cc8bc6
Tomer Admon <tomer.a@qualisystems.com>
2017-06-17 13:26:57 +0300

    Update cloudshell_api_example.py

--- cloudshell_api_example.py
+++ cloudshell_api_example.py
@@ -1,8 +1,11 @@
 from cloudshell.api.cloudshell_api import CloudShellAPISession
 import sys
+import logging
 
 server_ip = sys.argv[1] ##'172.31.22.239' ## This is the internal IP of our CloudShell in AWS
+print server_ip
 reservation_id = sys.argv[2]
+print reservation_id
 DEPLOYED_APP_MODEL = 'Generic Deployed App'
 
 session = CloudShellAPISession(server_ip,
@@ -12,10 +15,13 @@ session = CloudShellAPISession(server_ip,
 										  ##to pass these credentials from jenkins and don't store them in GitHub!!
 
 resources = session.GetReservationDetails(reservation_id).ReservationDescription.Resources
+
+print str(len(resources))
+
 my_resource = [resource for resource in resources
                if resource.ResourceModelName == DEPLOYED_APP_MODEL]
 
 if len(my_resource) > 1:
     raise Exception('There are more then one app in the sandbox')
 
-print my_resource[0].FullAddress 
+print (my_resource[0].FullAddress) 
