6c3dc07bc42b5322a21461a7b77933aec96f8ff5
Tomer Admon <tomer.a@qualisystems.com>
2018-01-03 08:56:58 +0200

    Update cloudshell_api_example.py

--- cloudshell_api_example.py
+++ cloudshell_api_example.py
@@ -1,7 +1,7 @@
 from cloudshell.api.cloudshell_api import CloudShellAPISession
 import sys
 
-server_ip = sys.argv[1] ##'172.31.22.239' ## This is the internal IP of our CloudShell in AWS
+server_ip = sys.argv[1] ##'10.0.0.55' ## This is the internal IP of our CloudShell in AWS
 reservation_id = sys.argv[2]
 DEPLOYED_APP_MODEL = 'Generic App Model'
 
