187778890edf9f39e689090a15df766956b2df3c
Tomer Admon <tomer.a@qualisystems.com>
2018-01-10 21:27:55 +0200

    Create logon.py

---
+++ logon.py
@@ -0,0 +1,7 @@
+from cloudshell.api.cloudshell_api import CloudShellAPISession
+import sys
+
+session = CloudShellAPISession(10.0.0.55,
+                               admin,
+                               admin,
+                               Global)  ##make sure 
