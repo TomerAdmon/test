86a119ed4ae128f904d7a919deee3d7f8a794c6a
Tomer Admon <tomer.a@qualisystems.com>
2018-01-13 17:42:35 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get("http://www.google.com")
 time.sleep(10)
